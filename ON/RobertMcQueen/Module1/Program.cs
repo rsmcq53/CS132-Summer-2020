763038fcc0ad11e41992e8716ff8e33fb01d30b7
B53 <67708075+rsmcq53@users.noreply.github.com>
2020-07-11 17:11:26 -0700

    Create Program.cs
    HOP01_RobertMcQueen

---
+++ ON/RobertMcQueen/Module1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Robert";
+            int Age = 45;
+            bool isFemale = false;
+            double MyFloat = 10.1;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isFemale);
+            Console.WriteLine(MyFloat);
+        }
+    }
+}
+
+
