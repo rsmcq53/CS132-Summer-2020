378639cceba74b613ce82d240a4598f290e2901c
B53 <67708075+rsmcq53@users.noreply.github.com>
2020-07-27 00:43:36 -0700

    HOP03_McQueenRobert

---
+++ ON/RobertMcQueen/Module3/Operators-Routines/Increment-Decrement/Program.cs
@@ -0,0 +1,29 @@
+﻿/********
+**
+** Name: McQueen, Robert
+** Class: CS 132
+** Project: HOP03
+** Date: 2020-07-26
+** Description: Increment and Decrement operators and their operations on variables and values.
+** 
+********/
+
+using System;
+using static System.Console;
+
+namespace Increment_Decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a <= 10; a++) // increments the values from 0 to 10, returning the values less than or equal to 10.
+                WriteLine(a);
+
+            for (int b = 15; b > 0; b--) // decrements b from 15 to 1 returning the value of all number greater than zero, ending the loop at 0.
+                WriteLine(b);
+
+
+        }
+    }
+}
---
+++ ON/RobertMcQueen/Module3/Operators-Routines/NumericOperators/Program.cs
@@ -0,0 +1,37 @@
+﻿/********
+**
+** Name: McQueen, Robert
+** Class: CS 132
+** Project: HOP03
+** Date: 2020-07-26
+** Description: Part 1, Numeric Operators
+** 
+********/
+
+using System;
+using static System.Console;
+
+namespace Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20;
+            int num2 = 77;
+            float num3 = 5.2f; // float is used due to the value of 5.2 not being of type int
+
+            float sum = num1 + num2 + num3; // Sum  20+77+5.2=102.2
+            int subtract = num2 - num1;     // Difference 77-20=57
+            int multiply = num1 * num2;     // Product 77*22=1540
+            float divide = num1 / num3;     // Quotient 20/5.2=3.846154  float divide is used in place of int due to num3 being a floating point number, int would require a an explicit cast which would result in a truncation after the decimal 
+            int modulus = num2 % num1;      // Remainder 77%20=17
+
+            WriteLine("Sum of 3 numbers is: " + sum);
+            WriteLine("77 - 20 is: " + subtract);
+            WriteLine("20 * 77 is: " + multiply);
+            WriteLine("20 / 5.2 is: " + divide);
+            WriteLine("77 % 20 is: " + modulus);
+        }
+    }
+}
